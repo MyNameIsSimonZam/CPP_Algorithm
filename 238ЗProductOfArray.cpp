879c7a69cb7667ad9f304a462e48bbfa415833e7
Simon <semazamesin@gmail.com>
2023-09-14 11:17:32 +0500

    update 238ЗProductOfArray.cpp

--- 238ЗProductOfArray.cpp
+++ 238ЗProductOfArray.cpp
@@ -3,18 +3,38 @@
 
 using namespace std;
 
+vector<int> productExceptSelf(vector<int>& nums) {
+	int res = 1;
+	for (int i = 0; i < nums.size(); i++) {
+		if (nums[i] != 0)
+			res = res * nums[i];
+	}
+	vector<int> answer(nums.size(), 0);
+	int countZero = 0;
+	for (int i = 0; i < nums.size(); i++) {
+		if (nums[i] == 0) countZero += 1;
+	}
+	if (countZero == 0) {
+		for (int i = 0; i < answer.size(); i++) {
+			answer[i] = res / nums[i];
+		}
+	}
+	if (countZero == 1) {
+		for (int i = 0; i < answer.size(); i++) {
+			if (nums[i] == 0)
+				answer[i] = res;
+		}
+	}
 
-//int main()
-//{
-//	vector<int> nums{ 1,2,3,4 };
-//	vector<int> res{};
-//
-//	for (int i = 0; i < nums.size(); i++)
-//	{
-//		for (int j = 0; j < nums.size(); j++)
-//		{
-//			vector<int> res{i} = 
-//		}
-//	}
-//
-//}
+	//for (int i = 0; i < answer.size(); i++) {
+	//	cout << answer[i] << " ";
+	//}
+	return answer;
+}
+
+int main2() {
+	vector<int> nums{ -1,1,0,-3,3,0 };
+	vector<int> answer = productExceptSelf(nums);
+
+	return 0;
+}
