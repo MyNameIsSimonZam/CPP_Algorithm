269a0a0f234117c9d669c9a75dc89968f3bd6905
Simon <semazamesin@gmail.com>
2023-09-29 12:59:55 +0500

    add_item_242.ValidAnagram

---
+++ 242.ValidAnagram.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+bool isAnagram(string s, string t) {
+
+}
+
+
+int main() {
+	string s = "anagram";
+	string t = "nagaram";
+
+	return 0;
+}
