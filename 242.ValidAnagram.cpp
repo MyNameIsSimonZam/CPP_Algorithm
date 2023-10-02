766be8603fa5aafef3d7fec70e339d7da8754af9
Simon <semazamesin@gmail.com>
2023-10-02 11:52:47 +0500

    update_242.ValidAnagram

--- 242.ValidAnagram.cpp
+++ 242.ValidAnagram.cpp
@@ -1,51 +1,63 @@
-//#include<iostream>
-//#include<string>
-//#include<unordered_set>
-//
-//using namespace std;
-//
-//
-//bool isAnagram(string s, string t) {
-//	if (s.size() != t.size()) return false;
-//	unordered_set<char> a(s.begin(), s.end());
-//	for (int i = 0; i < t.size(); i++) {
-//		if (a.count(t[i])) {
-//			a.erase(t[i]);
-//		}
-//		else {
-//			return false;
-//		}
-//	}
-//	return true;
-//}
-//
-//
-//int main() {
-//	string s = "anagram";
-//	string t = "nagaram";
-//	cout << isAnagram(s, t);
-//	return 0;
-//}
+#include<iostream>
+#include<string>
+#include<unordered_map>
+
+using namespace std;
+
+// speed O(n), space O(26) = O(1)
+bool isAnagram(string s, string t) {
+	if (s.size() != t.size()) return false;
+	unordered_map<char, int> mp;
+	for (int i = 0; i < s.size(); i++) {
+		if (mp.count(s[i])) {
+			mp[s[i]]++;
+		}
+		else {
+			mp[s[i]] = 1;
+		}
+	}
+	for (size_t i = 0; i < t.size(); i++)
+	{
+		if (mp.count(t[i])) {
+			mp[t[i]]--;
+			if (mp[t[i]] < 0) {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+	return true;
+}
+
+
+int main() {
+	string s = "ccac";
+	string t = "caac";
+	cout << isAnagram(s, t);
+	return 0;
+}
 
 
 
 //__sort_and_comparison__O(n log n)
 
-#include<iostream>
-#include<string>
-#include <algorithm>
-
-using namespace std;
-
-
-bool isAnagram(string s, string t) {
-	if (s.size()!= t.size()) return false;
-	sort(s.begin(), s.end());
-	sort(t.begin(), t.end());
-	for (int i = 0; i < s.size(); i++)
-		if (s[i] != t[i]) return false;
-	return true;
-}
+//#include<iostream>
+//#include<string>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//
+//bool isAnagram(string s, string t) {
+//	if (s.size()!= t.size()) return false;
+//	sort(s.begin(), s.end());
+//	sort(t.begin(), t.end());
+//	for (int i = 0; i < s.size(); i++)
+//		if (s[i] != t[i]) return false;
+//	return true;
+//}
 
 
 //int main() {
@@ -53,4 +65,22 @@ bool isAnagram(string s, string t) {
 //	string t = "nagaram";
 //	cout << isAnagram(s, t);
 //	return 0;
+//}
+
+
+
+// сокращенная запись mp++
+//bool isAnagram(string s, string t) {
+//	if (s.size() != t.size()) return false;
+//	unordered_map<char, int> mp;
+//	for (int i = 0; i < s.size(); i++) {
+//		mp[s[i]]++;
+//	}
+//	for (size_t i = 0; i < t.size(); i++) {
+//		mp[t[i]]--;
+//		if (mp[t[i]] < 0) {
+//			return false;
+//		}
+//	}
+//	return true;
 //}
--- 387.FirstUniqueCharacterinaString.cpp
+++ 387.FirstUniqueCharacterinaString.cpp
@@ -1,12 +1,31 @@
 #include<iostream>
 #include<string>
+#include<algorithm>
 
 using namespace std;
 
+
 int firstUniqChar(string s) {
 
+	for (int i = 0; i < s.size(); i++) {
+		int temp = 0;
+		for (int j = 0; j < s.size(); j++)
+		{
+			if (s[i] == s[j]) {
+				temp++;
+			}
+		}
+		if (temp == 1) {
+			return i;
+		}
+	}
+	return -1;
 }
 
-int main() {
 
-}
+
+//int main() {
+//	string s = "loveleetcode";
+//	cout << firstUniqChar(s);
+//	return 0;
+//}
