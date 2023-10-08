81da9cfe6ad64fbb8961eff4ea138caeb8d40adf
Simon <semazamesin@gmail.com>
2023-10-08 17:19:31 +0500

    add_solution_26.RemoveDuplicatesfromSortedArray

--- 26.RemoveDuplicatesfromSortedArray.cpp
+++ 26.RemoveDuplicatesfromSortedArray.cpp
@@ -1,15 +1,34 @@
+#include<iostream>
 #include<vector>
 
 using namespace std;
 
 int removeDuplicates(vector<int>& nums) {
-	
+	int l = nums[0];
+	int r = nums[1];
+	int count = 1;
+	while (r < nums.size() - 1) {
+		if (nums[l] == nums[r]) {
+			r++;
+		}
+		if (nums[l] != nums[r]) {
+			l++;
+			count++;
+			nums[l] = nums[r];
+		}
+	}
 
-	
-	return 0;
+	return count;
 }
 
 int main() {
+	vector<int> nums = { 0, 0, 0, 1, 1, 1, 2, 3, 3, 4 };
+	int k = removeDuplicates(nums);
+	cout << k << '\n';
+	for (int n : nums) {
+		cout << n << ' ';
+	}
+	cout << '\n';
 
 	return 0;
 }
