3710386b1612cfe37f3fb786ebf6d5b399df6c70
Simon <semazamesin@gmail.com>
2023-10-10 19:42:12 +0500

    add_new_objects_11.ContainerWithMostWater_and_202.HappyNumber

---
+++ 11.ContainerWithMostWater.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int maxArea(vector<int>& height) {
+	return 0;
+}
+
+
+int main() {
+	return 0;
+}
---
+++ 202.HappyNumber.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+bool isHappy(int n) {
+
+}
+
+
+//int main() {
+//	return 0;
+//}
