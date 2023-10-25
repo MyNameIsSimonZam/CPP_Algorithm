f913669a474d8ed55907c293cf8e50facccfd6f2
Simon <semazamesin@gmail.com>
2023-10-25 15:23:53 +0500

    THE_END

--- 202.HappyNumber.cpp
+++ 202.HappyNumber.cpp
@@ -2,27 +2,25 @@
 
 using namespace std;
 
-bool isHappy(int n) {
-
-	int digit = 1;
+int sumSquare(int n) {
 	int res = 0;
-	int count = 0;
-
-	while (res != 1 && count != 1000000) {
-		while (n != 0) {
-			digit = n % 10;
-			n = n / 10;
-			res += digit * digit;
-		}
-		if (res != 1) {
-			n = res;
-			res = 0;
-			digit = 1;
-		}
-		count++;
+	while (n != 0) {
+		int digit = n % 10;
+		n = n / 10;
+		res += digit * digit;
 	}
-	if (res == 1) return true;
-	else return false;
+	return res;
+}
+
+bool isHappy(int n) {
+	int l = n;
+	int r = sumSquare(n);
+	while (r != 1 && l != r) {
+		l = sumSquare(l);
+		r = sumSquare(r);
+		r = sumSquare(r);
+	}
+	return r == 1;
 }
 
 
