5411808a5c0bbd95f3e429e8de0ff12b1853e343
Simon <semazamesin@gmail.com>
2023-10-30 21:00:39 +0500

    that_is_end

--- 21.MergeTwoSortedLists.cpp
+++ 21.MergeTwoSortedLists.cpp
@@ -9,6 +9,14 @@ struct ListNode {
 	ListNode() : val(0), next(nullptr) {}
 	ListNode(int x) : val(x), next(nullptr) {}
 	ListNode(int x, ListNode* next) : val(x), next(next) {}
+	void print() {
+		ListNode* currentNode = this;
+		while (currentNode != nullptr) {
+			cout << currentNode->val << ' ';
+			currentNode = currentNode->next;
+		}
+		cout << '\n';
+	}
 };
 
 struct List {
@@ -26,7 +34,8 @@ struct List {
 			last = newNode;
 		}
 	}
-	void print(ListNode* currentNode) {
+	void print() {
+		ListNode* currentNode = head;
 		while (currentNode != nullptr) {
 			cout << currentNode->val << ' ';
 			currentNode = currentNode->next;
@@ -37,26 +46,36 @@ struct List {
 
 
 
-//ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-//	if (list1 == nullptr && list2 == nullptr) return nullptr;
-//	ListNode* node1 = list1;
-//	ListNode* node2 = list2;
-//	ListNode* ansverNode;
-//	if (node1 > node2) {
-//		ListNode* ansverNode = node2;
-//	}
-//	else {
-//		ListNode* ansverNode = node1;
-//	}
-//	while (node1 != nullptr && node2 != nullptr) {
-//		if (node1 > node2) {
-//			ansverNode->next = node2;
-//		}
-//		else if (node1 < node2) {
-//			ansverNode->next = node1;
-//		}
-//	}
-//}
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+	//if (list1 == nullptr && list2 == nullptr) return nullptr;
+	ListNode* node1 = list1;
+	ListNode* node2 = list2;
+	List* ansverList = new List();
+
+	while (node1 != nullptr && node2 != nullptr) {
+		if (node1->val > node2->val) {
+			ansverList->append(node2->val);
+			node2 = node2->next;
+		}
+		else {
+			ansverList->append(node1->val);
+			node1 = node1->next;
+		}
+	}
+	if (node1 != nullptr) {
+		while (node1 != nullptr) {
+			ansverList->append(node1->val);
+			node1 = node1->next;
+		}
+	}
+	if (node2 != nullptr) {
+		while (node2 != nullptr) {
+			ansverList->append(node2->val);
+			node2 = node2->next;
+		}
+	}
+	return ansverList->head;
+}
 
 int main() {
 	List myList1;
@@ -69,8 +88,11 @@ int main() {
 	myList2.append(4);
 	myList2.append(5);
 
-	myList1.print(myList1.head);
-	myList2.print(myList2.head);
+	myList1.print();
+	myList2.print();
+
+	ListNode* newList = mergeTwoLists(myList1.head, myList2.head);
+	newList->print();
 
 	return 0;
 }
